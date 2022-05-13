bb41cb434f7df8b4258230becf878415e078ee43
SRNissen <sorennissen@gmail.com>
2022-05-13 11:20:33 +0200

    Add project files.

---
+++ msvc_test/msvc_test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace msvctest
+{
+	TEST_CLASS(msvctest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
